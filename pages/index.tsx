428f9b438e2258ab7e606effc708bc193e8f10a9
Sebastian <sebastian.sanchez@tecsup.edu.pe>
2022-03-07 14:03:04 -0500

    feat: add src folder and add material ui

--- pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import type { NextPage } from 'next'
-
-const HomePage: NextPage = () => {
-  return (
-    <div>Home Page</div>
-  )
-}
-
-export default HomePage
---
+++ src/pages/_app.tsx
@@ -0,0 +1,17 @@
+import type { AppProps } from 'next/app'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+
+import { darkTheme, lightTheme } from '../themes'
+
+import '../styles/globals.css'
+
+function MyApp({ Component, pageProps }: AppProps) {
+    return (
+        <ThemeProvider theme={darkTheme}>
+            <CssBaseline />
+            <Component {...pageProps} />
+        </ThemeProvider>
+    )
+}
+
+export default MyApp
